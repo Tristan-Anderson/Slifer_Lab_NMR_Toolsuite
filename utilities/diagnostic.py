bdfec331de0febd9eb37b27be309d2b2bdb6a1a7
Tristan Anderson <tris31299@gmail.com>
2021-05-29 11:54:16 -0400

    Added diagnostic file and sample dataset.

---
+++ utilities/diagnostic.py
@@ -0,0 +1,18 @@
+import matplotlib, pandas, scipy, os, gc, time, multiprocessing, sys, matplotlib, argparse, numpy, math, traceback, datetime, warnings
+sys.path.insert(1, '..')
+import variablenames,daq_muncher, directory_sorter, sweep_averager, global_interpreter, spin_extractor, NMR_Analyzer
+
+samplefile = 'sampledataset.csv'
+sampledumpdir = 'diagnostic_dir/'
+home = os.get_cwd()
+root = os.get_cwd() + '/'+sampledumpdir
+tedir = root +'/TE/'
+baselinedir = root+'/Baseline/'
+blfile = baselinedir+'baseline_average.ta1'
+polarizationdir = root+'/Polarization/'
+daqfile = root + samplefile
+daq_muncher.single_file(daqfile, root)
+directory_sorter.shelf(tedir,seconds=30)
+sweep_averager.avg_nested_dirs(tedir)
+sweep_averager.avg_single_dir(baselinedir)
+
