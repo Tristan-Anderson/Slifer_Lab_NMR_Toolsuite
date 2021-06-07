000a3d7f284681e0471157f5d68fd9e7e25e5520
Tristan Anderson <tris31299@gmail.com>
2021-06-07 09:49:39 -0400

    push mods to laptop. mobile work time.

--- utilities/diagnostic.py
+++ utilities/diagnostic.py
@@ -1,3 +1,18 @@
+"""
+Tristan Anderson
+takc1nqa@gmail.com
+tanderson@vt.edu
+"""
+
+
+
+# Fortune
+"""
+I love ROCK 'N ROLL!  I memorized all the WORDS to "WIPE-OUT" in
+1965!!
+"""
+
+
 import matplotlib, pandas, scipy, os, gc, time, multiprocessing, sys, matplotlib, argparse, numpy, math, traceback, datetime, warnings
 sys.path.insert(1, '..')
 import variablenames,daq_muncher, directory_sorter, sweep_averager, global_interpreter, spin_extractor, NMR_Analyzer
