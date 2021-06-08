e100f5b9821ae2574db2a728a1f34db699cfc0a7
Tristan Anderson <tris31299@gmail.com>
2021-06-08 13:49:47 -0400

    fixed a weird bug where dir sorter suddenly forgot it needed to have a forward slash at the ends of the directory paths. Could have been an issue with a past merge. I blame myself.

--- directory_sorter.py
+++ directory_sorter.py
@@ -32,6 +32,9 @@ def shelf(location, **ts):
 	into tiny directories, the timespan of which
 	is regulated by the user
 	"""
+	location += '/' # needs to be marked as a directory
+
+
 	hours = ts.pop("hours",0)
 	minutes = ts.pop("minutes",0)
 	seconds = ts.pop("seconds",0)
@@ -49,17 +52,18 @@ def shelf(location, **ts):
 	for file in fnames:
 		# Extract all of the dates out of the filenames in the directory.
 		alldates.append(datetime.datetime.strptime(''.join(file.split('.')[0].split('_')[1:]),'%Y%m%d%H%M%S'))
-
+	
 	minalldates = min(alldates)
-	prevstepalldates = minalldates
-	stepalldates = minalldates+timestep
-	while prevstepalldates < max(alldates):
+	start_of_daterange = minalldates
+	end_of_daterange = minalldates+timestep
+	while start_of_daterange < max(alldates):
+		#print('stepping into loop: 58: ', location, '\nStart of date range:', start_of_daterange, 'end of daterange:', end_of_daterange)
 		os.chdir(location)
 
 		#Form the naming for the directory
-		ampm = datetime.datetime.strftime(stepalldates, "%p")
+		ampm = datetime.datetime.strftime(end_of_daterange, "%p")
 		ap = "p" if ampm == "PM" else "a"
-		#directoryname = datetime.datetime.strftime(prevstepalldates,"%m%d_%H%M%S_")+datetime.datetime.strftime(stepalldates,"%H%M%S")+ap+'/'
+		#directoryname = datetime.datetime.strftime(start_of_daterange,"%m%d_%H%M%S_")+datetime.datetime.strftime(end_of_daterange,"%H%M%S")+ap+'/'
 
 		
 
@@ -68,11 +72,13 @@ def shelf(location, **ts):
 		subsection = []
 		for index, date in enumerate(alldates):
 			# If the specific date is within the correct range
-			if date >= prevstepalldates and date <= stepalldates:
+			if date >= start_of_daterange and date <= end_of_daterange:
 				# collect a subsection
 				subsection.append(date)
 				# add the subsection to the list of files to move.
 				files_to_move.append(datetime.datetime.strftime(date, "%Y_%m_%d_%H_%M_%S"))
+
+		#print('79: files to move:', files_to_move)
 		if len(files_to_move) > 0:
 			directoryname = datetime.datetime.strftime(min(subsection),"%m%d_%H%M%S_")+datetime.datetime.strftime(max(subsection),"%m%d_%H%M%S")+ap+'/'
 
@@ -89,14 +95,15 @@ def shelf(location, **ts):
 				#print(glob.glob(location+'*'+f+'*'), f)
 				try:
 					actual_file = glob.glob(location+'*'+f+'*')[0]
-				except IndexError:
+				except IndexError as e:
 					# If she ain't were she's supposed to be, she's been moved....
+					print(e, f)
 					#print(f, "has already been moved.")
 					continue
 				actual_file_name = actual_file.split('/')[-1]
 				shutil.move(actual_file, location+directoryname+actual_file_name)
-		prevstepalldates = stepalldates
-		stepalldates += timestep
+		start_of_daterange = end_of_daterange
+		end_of_daterange += timestep
 	print("Shelving complete.")
 
 def unshelf(location):
@@ -105,17 +112,20 @@ def unshelf(location):
 	children of the "location" directory, and move
 	them to the parent/root/"location" directory.
 	"""
+	location += '/' # needs to be marked as a directory
+
 	home = os.getcwd()
 	os.chdir(location)
 	for (dirpath, dirnames, filenames) in os.walk(location):
 		dirnames = dirnames
 		break
 	for child in dirnames:
-		d = location+child
+		d = location+child+'/'
+		print(d)
 		os.chdir(d)
 		for (dirpath, dirnames, filenames) in os.walk(d):
 			for file in filenames:
-				shutil.move(d+'/'+file, location+'/'+file)
+				shutil.move(d+file, location+file)
 			break
 		os.chdir(location)
 		os.rmdir(d)
